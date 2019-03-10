b3965d2c7fcd8d98409cf0ac1135f46219bc4691
Guilherme França <guii_real@hotmail.com>
2019-03-10 19:13:19 -0300

    Projeto Exportador CSV

--- modulo06/exportador_csv/index.js
+++ modulo06/exportador_csv/index.js
@@ -3,20 +3,22 @@ const fs = require('fs')
 
 const sql = 'SELECT ID, NOME, CARGO FROM PESSOA'
 const writable = fs.createWriteStream('pessoas.csv')
-
-connection.connect(err => {
-    if (err) console.log(`Erro ao conectar ao banco de dados ! Erro: ${err}`)
-    const query = connection.query(sql)
-    query.on('result', row => {
-        const data = `${row.ID}, ${row.NOME}\n` 
-        connection.pause()
-        writable.write(data, () => {
-            connection.resume()
-            console.log('Written')
+writable.write(`ID,NOME\n`, () => {
+    connection.connect(err => {
+        if (err) console.log(`Erro ao conectar ao banco de dados ! Erro: ${err}`)
+        const query = connection.query(sql)
+        query.on('result', row => {
+            const data = `${row.ID},${row.NOME}\n` 
+            connection.pause()
+            writable.write(data, () => {
+                connection.resume()
+                console.log('Written')
+            })
+        })
+        query.on('end', () => {
+            writable.end()
+            connection.end()
         })
     })
-    query.on('end', () => {
-        writable.end()
-        connection.end()
-    })
 })
+
